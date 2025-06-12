6e14f43259525ee4ddc9c80982fb5f5752634cd6
joao-asoares <joaoasoares17@gmail.com>
2025-06-12 20:22:52 -0300

    Initial commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+export PYTHONPATH="$PYTHONPATH:$(pwd)"
+
+# conda create -n teste python=3.8
+# conda activate teste
+
+# pip install numpy==1.23.5
+# pip install -U scikit-multiflow pandas 
+
+python experiments/main.py
